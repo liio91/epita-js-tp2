bc89fa9016bb4be777a8884754634890a1a2b1da
Quentin Pré <pre.quentin@gmail.com>
2018-05-29 09:03:40 +0200

    [ADD] game basics

---
+++ src/components/Cell.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const cellStyle = {
+  display: "block",
+  backgroundColor: "white",
+  width: "200px",
+  height: "200px",
+  border: "1px solid #333",
+  outline: "none",
+  textAlign: "center",
+  lineHeight: "200px",
+  cursor: "pointer"
+};
+
+const Cell = () => <div style={cellStyle}>?</div>;
+
+export default Cell;
